e72439aa90f12d1de5c77e1c7848907ce7d327ba
Darklinio <linio_neo@hotmail.fr>
2014-03-06 14:02:08 +0100

    Remove AGF(too many change to use it)

--- src/fr/jc_android/gameframework/Audio.java
+++
@@ -1,7 +0,0 @@
-package fr.jc_android.gameframework;
-
-public interface Audio {
-    public Music createMusic(String file);
-
-    public Sound createSound(String file);
-}
--- src/fr/jc_android/gameframework/Sound.java
+++
@@ -1,7 +0,0 @@
-package fr.jc_android.gameframework;
-
-public interface Sound {
-    public void play(float volume);
-
-    public void dispose();
-}
